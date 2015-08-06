cb9ff9e5394535021390f0ab627b3c8f433a3ce9
Glamdring <glamd@abv.bg>
2015-08-06 13:19:28 +0300

    more information about the file functionality; removed absolute paths

---
+++ extras/excel-csv-export/export-csv.php
@@ -0,0 +1,12 @@
+﻿<?php
+header("Content-Type: text/html;charset=utf-8");
+?>
+<html>
+<body>
+След като сте запазили xls файлът като .txt чрез "Save as Unicode (.txt)", качете получения .txt файл, за да го конвертирате към csv:
+<form action="export.php" method="POST" enctype="multipart/form-data">
+<input type="file" name="sourceFile" />
+<input type="submit" value="Изпрати" />
+</form>
+</body>
+</html>

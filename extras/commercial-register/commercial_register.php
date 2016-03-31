e49329f2b867afffda5d72fbc5a0a8db1f5caf74
Bozhidar Bozhanov <glamd@abv.bg>
2016-03-31 10:17:29 +0300

    Създаване на файл за получаване на последните промени от търговския регистър

---
+++ extras/commercial-register/commercial_register.php
@@ -0,0 +1,8 @@
+<?php
+$datastr = fopen('php://input',rb);
+if ($fp = fopen('/data/comreg/dump' . date('m-d-Y') . '.xml', "wb")){
+    while(!feof($datastr)){
+        fwrite($fp,fread($datastr,4096)) ;
+    }
+}
+?>
